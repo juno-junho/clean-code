12bda061a3729bd6902ed11e34776fb42139ca2c
juno-junho <ssmm0205@naver.com>
2022-11-28 17:31:41 +0900

    refactor: preserve whole object, getRate메서드 위치 이동

--- refactoring/src/main/java/me/whiteship/refactoring/_03_long_function/_09_preserve_whole_object/Participant.java
+++ refactoring/src/main/java/me/whiteship/refactoring/_03_long_function/_09_preserve_whole_object/Participant.java
@@ -13,4 +13,12 @@ public record Participant(String username, Map<Integer, Boolean> homework) {
     }
 
 
+    // 1. preserve whole object 후 고려해야 할 것이 각각의 메서드가 과연 participant에 의존하는것이 맞는가. Hashmap을 보고 있는것이 맞는가. 한번쯤 고려해봐야함.
+    // 2. 과연 이 메서드는 이 위치가 적절한가. totalNumberOfEvents을 parameter로 전달해서 getRate를 구할수 있을지 않을까. (Participant로 이동)
+    double getRate(int totalNumberOfEvents) {
+        long count = homework().values().stream()
+                .filter(v -> v == true)
+                .count();
+        return (double) (count * 100 / totalNumberOfEvents);
+    }
 }
