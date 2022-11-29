36b259c3ae04315d8217dda561099a9cc58729d5
juno-junho <ssmm0205@naver.com>
2022-11-29 19:19:07 +0900

    refactor: encapsulate variable
    전역으로 데이터가 선언되어 있는 경우 변수 캡슐화 하기

--- refactoring/src/main/java/me/whiteship/refactoring/_05_global_data/_17_encapsulate_variable/Thermostats.java
+++ refactoring/src/main/java/me/whiteship/refactoring/_05_global_data/_17_encapsulate_variable/Thermostats.java
@@ -1,13 +1,45 @@
 package me.whiteship.refactoring._05_global_data._17_encapsulate_variable;
 
 public class Thermostats {
+    private static Integer targetTemperature = 70;
 
-    public static Integer targetTemperature = 70;
+    private static Boolean heating = true;
 
-    public static Boolean heating = true;
+    private static Boolean cooling = false;
 
-    public static Boolean cooling = false;
+    private static Boolean readInFahrenheit = true;
 
-    public static Boolean readInFahrenheit = true;
+    public static Integer getTargetTemperature() {
+        return targetTemperature;
+    }
 
+    public static void setTargetTemperature(Integer targetTemperature) {
+        Thermostats.targetTemperature = targetTemperature;
+    }
+
+    public static Boolean getHeating() {
+        return heating;
+    }
+
+    public static void setHeating(Boolean heating) {
+        // TODO validation logic
+        Thermostats.heating = heating;
+        // TODO notify (바뀌면 알림오게 하거나 등등..)
+    }
+
+    public static Boolean getCooling() {
+        return cooling;
+    }
+
+    public static void setCooling(Boolean cooling) {
+        Thermostats.cooling = cooling;
+    }
+
+    public static Boolean getReadInFahrenheit() {
+        return readInFahrenheit;
+    }
+
+    public static void setReadInFahrenheit(Boolean readInFahrenheit) {
+        Thermostats.readInFahrenheit = readInFahrenheit;
+    }
 }
