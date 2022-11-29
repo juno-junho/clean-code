2ae65a6ca0d0e53b6a31b0f3ae70aaaa3c6e2b42
juno-junho <ssmm0205@naver.com>
2022-11-29 19:54:13 +0900

    refactor: split variable
    변수 쪼개기

--- refactoring/src/main/java/me/whiteship/refactoring/_06_mutable_data/_18_split_variable/Order.java
+++ refactoring/src/main/java/me/whiteship/refactoring/_06_mutable_data/_18_split_variable/Order.java
@@ -3,8 +3,14 @@ package me.whiteship.refactoring._06_mutable_data._18_split_variable;
 public class Order {
 
     public double discount(double inputValue, int quantity) {
-        if (inputValue > 50) inputValue = inputValue - 2;
-        if (quantity > 100) inputValue = inputValue - 1;
-        return inputValue;
+        double result = inputValue;
+//        if (inputValue > 50) inputValue = inputValue - 2;
+        if (inputValue > 50) return result -= 2;
+//        if (quantity > 100) inputValue = inputValue - 1;
+        if (quantity > 100) return result -= 1;
+        //입력 값이 return 값으로 재사용됨.
+//        return inputValue;
+        return result;
     }
+
 }
--- refactoring/src/main/java/me/whiteship/refactoring/_06_mutable_data/_18_split_variable/Rectangle.java
+++ refactoring/src/main/java/me/whiteship/refactoring/_06_mutable_data/_18_split_variable/Rectangle.java
@@ -6,13 +6,17 @@ public class Rectangle {
     private double area;
 
     public void updateGeometry(double height, double width) {
-        double temp = 2 * (height + width);
-        System.out.println("Perimeter: " + temp);
-        perimeter = temp;
+        // 여기서 temp 변수가 여러번 할당 됨.
+        // 1. 지름을 구할 때, 2. 넓이를 구할 때 둘다 사용.
+//        double temp = 2 * (height + width);
+        final double perimeter = 2 * (height + width);
+        System.out.println("Perimeter: " + perimeter);
+        this.perimeter = perimeter;
 
-        temp = height * width;
-        System.out.println("Area: " + temp);
-        area = temp;
+//        temp = height * width;
+        final double area = height * width;
+        System.out.println("Area: " + area);
+        this.area = area;
     }
 
     public double getPerimeter() {
