3979ce65f31f36bed50dbf33812a1789527429c4
juno-junho <ssmm0205@naver.com>
2022-11-29 19:57:45 +0900

    refactor: split variable
    변수 쪼개기

--- refactoring/src/main/java/me/whiteship/refactoring/_06_mutable_data/_18_split_variable/Order.java
+++ refactoring/src/main/java/me/whiteship/refactoring/_06_mutable_data/_18_split_variable/Order.java
@@ -5,9 +5,9 @@ public class Order {
     public double discount(double inputValue, int quantity) {
         double result = inputValue;
 //        if (inputValue > 50) inputValue = inputValue - 2;
-        if (inputValue > 50) return result -= 2;
+        if (inputValue > 50) result -= 2;
 //        if (quantity > 100) inputValue = inputValue - 1;
-        if (quantity > 100) return result -= 1;
+        if (quantity > 100) result -= 1;
         //입력 값이 return 값으로 재사용됨.
 //        return inputValue;
         return result;
