fcea7f4bf53ff5399f0023ede81178cfaffccf17
juno-junho <ssmm0205@naver.com>
2022-11-29 18:20:53 +0900

    refactor: replace paramter with query
    매개변수를 질의 함수로 바꾸기

--- refactoring/src/main/java/me/whiteship/refactoring/_04_long_parameter_list/_14_replace_parameter_with_query/Order.java
+++ refactoring/src/main/java/me/whiteship/refactoring/_04_long_parameter_list/_14_replace_parameter_with_query/Order.java
@@ -1,5 +1,6 @@
 package me.whiteship.refactoring._04_long_parameter_list._14_replace_parameter_with_query;
 
+// 질의 함수 : 연산을 통해 계산하여 반환하는 함수
 public class Order {
 
     private int quantity;
@@ -13,11 +14,16 @@ public class Order {
 
     public double finalPrice() {
         double basePrice = this.quantity * this.itemPrice;
-        int discountLevel = this.quantity > 100 ? 2 : 1;
-        return this.discountedPrice(basePrice, discountLevel);
+        return this.discountedPrice(basePrice);
     }
 
-    private double discountedPrice(double basePrice, int discountLevel) {
-        return discountLevel == 2 ? basePrice * 0.90 : basePrice * 0.95;
+    private int discountLevel() {
+        return this.quantity > 100 ? 2 : 1;
+    }
+
+    //discountLevel 계산하는 것이 함수 자체의 책임이 됨.
+    // 전달해야할 parameter 하나로 줌.
+    private double discountedPrice(double basePrice) {
+        return discountLevel() == 2 ? basePrice * 0.90 : basePrice * 0.95;
     }
 }
